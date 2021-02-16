4e32ec47f1784e664a937ef6cfcd732b6890b220
WCour <WCour@192.168.1.83>
2021-02-16 01:55:14 -0700

    Setting up file structure based on assignment doc

---
+++ src/java/models/AccountService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author WCour
+ */
+public class AccountService {
+    
+    public String user;
+    public String pass;
+    
+    
+    public AccountService(String user,String pass){
+        this.user = user;
+        this.pass = pass;
+    }
+          
+}
---
+++ web/WEB-INF/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : Feb 16, 2021, 1:50:14 AM
+    Author     : WCour
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
